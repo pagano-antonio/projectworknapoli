0e9c5f55cacc18fdb60947de90dc16e1443b6e87
pagano.antonio <iatix1@gmail.com>
2024-02-29 15:29:57 +0100

    hh

---
+++ ProjectWorkNapoliBE/src/main/java/com/ProjectWorkNapoliBeApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWorkNapoliBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWorkNapoliBeApplication.class, args);
+	}
+
+}
