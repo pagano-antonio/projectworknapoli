2f71c9e3fa0f3cdfdb6ddac7b7b53d6f716ee10a
ros3nyx <vasilika.skura@gmail.com>
2024-03-05 15:01:35 +0100

    repository

---
+++ ProjectWorkNapoliBE/src/main/java/com/dao/StateJobInterviewRepository.java
@@ -0,0 +1,9 @@
+package com.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.model.StateJobInterview;
+
+public interface StateJobInterviewRepository extends JpaRepository<StateJobInterview, Integer>{
+
+}
---
+++ ProjectWorkNapoliBE/src/main/java/com/dao/WorkExperienceRepository.java
@@ -0,0 +1,9 @@
+package com.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.model.WorkExperience;
+
+public interface WorkExperienceRepository extends JpaRepository<WorkExperience, Integer>{
+
+}
