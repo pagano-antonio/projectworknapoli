8feaf9427c1c86451ff965787a37992418baa789
clariserni <clarissaserni.96@gmail.com>
2024-03-05 15:16:07 +0100

    repository nuove

---
+++ ProjectWorkNapoliBE/src/main/java/com/dao/CandidateCommercialDataRepository.java
@@ -0,0 +1,10 @@
+package com.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import com.model.CandidateCommercialData;
+
+public interface CandidateCommercialDataRepository extends JpaRepository<CandidateCommercialData, Integer>{
+
+}
