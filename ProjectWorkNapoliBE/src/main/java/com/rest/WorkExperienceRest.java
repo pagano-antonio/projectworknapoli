632327eb5f3ab1ba0252fdbcd1f75e38f7db9332
ros3nyx <vasilika.skura@gmail.com>
2024-03-05 18:04:43 +0100

    rest

---
+++ ProjectWorkNapoliBE/src/main/java/com/rest/WorkExperienceRest.java
@@ -0,0 +1,59 @@
+package com.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dao.WorkExperienceRepository;
+import com.model.WorkExperience;
+
+@RestController
+@RequestMapping("/work_experience")
+public class WorkExperienceRest {
+    @Autowired
+    private WorkExperienceRepository workExperienceRep;
+    
+    @GetMapping("ricercaById/{id}")
+    @ResponseBody
+    public WorkExperience findById(@PathVariable("id") int id) {
+    
+    	WorkExperience res = workExperienceRep.findById(id).get();
+    	
+    	return res;
+    	
+    }
+    
+    @PostMapping
+    public String add(@RequestBody WorkExperience w) {
+
+    	workExperienceRep.save(w);
+        System.out.println("Inserito nel db:"+w.getId());
+        return "OK";
+    }
+    
+    @PutMapping
+    public String update(@RequestBody WorkExperience w) {
+
+    	workExperienceRep.save(w);
+        System.out.println("Aggiornato nel db");
+        return "OK";
+    }
+    
+    @DeleteMapping("deleteById/{id}")
+    public void delete(@PathVariable("id") Integer id) {
+
+    	workExperienceRep.deleteById(id);
+        System.out.println("Eliminato dal db");
+        
+        
+    }
+    
+    
+}
