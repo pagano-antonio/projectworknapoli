376ab4bf6e13f07defe8cca19d5f708a46c601cd
Hellense <hellense@hotmail.it>
2024-03-06 10:10:01 +0100

    prova 2

--- ProjectWorkNapoliBE/src/main/java/com/dao/CandidateCommercialDataRepository.java
+++ ProjectWorkNapoliBE/src/main/java/com/dao/CandidateCommercialDataRepository.java
@@ -2,9 +2,9 @@ package com.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 import com.model.CandidateCommercialData;
 
-public interface CandidateCommercialDataRepository extends JpaRepository<CandidateCommercialData, Integer>{
+// commento di prova
+public interface CandidateCommercialDataRepository extends JpaRepository<CandidateCommercialData, Integer> {
 
 }
