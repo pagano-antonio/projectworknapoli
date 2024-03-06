c39945b40b6ca846cd015b53485dce896227b361
ros3nyx <vasilika.skura@gmail.com>
2024-03-06 11:02:01 +0100

    ultime rest

---
+++ ProjectWorkNapoliBE/src/main/java/com/rest/SkillRest.java
@@ -0,0 +1,59 @@
+package com.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dao.SkillRepository;
+import com.model.Skill;
+
+@RestController
+@RequestMapping("/skill")
+public class SkillRest {
+	
+	@Autowired
+	private SkillRepository skillRep;
+	
+	@GetMapping("findById/{id}")
+	@ResponseBody
+	public Skill findById(@PathVariable("id") int id) {
+
+		Skill res =skillRep.findById(id).get();
+
+		return res;
+
+	}
+
+	@PostMapping
+	public String add(@RequestBody Skill s) {
+
+		skillRep.save(s);
+		System.out.println("Inserito nel db:" + s.getIdSkill());
+		return "OK";
+	}
+
+	@PutMapping
+	public String update(@RequestBody Skill s) {
+
+		skillRep.save(s);
+		System.out.println("Aggiornato nel db");
+		return "OK";
+	}
+
+	@DeleteMapping("deleteById/{id}")
+	public void delete(@PathVariable("id") Integer id) {
+
+		skillRep.deleteById(id);
+		System.out.println("Eliminato dal db");
+
+	}
+
+
+}
