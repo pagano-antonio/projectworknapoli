f5d4091c55da0ede5a1d6b14e169d6028fb31dc8
Hellense <hellense@hotmail.it>
2024-03-07 18:03:36 +0100

    template + navbar

---
+++ ProjectWorkNapoliBE/src/main/java/com/ctr/Home.java
@@ -0,0 +1,16 @@
+package com.ctr;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Home {
+
+	@GetMapping("/gestionaleCandidati")
+	public String home(Model model) {
+		System.out.println(model);
+		return "home";
+	}
+
+}
