787904076fee870c5df78a8dfabd28c5713a49cb
Hellense <hellense@hotmail.it>
2024-03-28 12:29:15 +0100

    abilitato angular anche con auth filter

--- ProjectWorkNapoliBE/src/main/java/com/config/AuthenticationFilter.java
+++ ProjectWorkNapoliBE/src/main/java/com/config/AuthenticationFilter.java
@@ -1,28 +1,39 @@
-/*
- * package com.config;
- * 
- * import java.io.IOException;
- * 
- * import org.springframework.context.annotation.Configuration; import
- * org.springframework.web.filter.OncePerRequestFilter;
- * 
- * import jakarta.servlet.FilterChain; import jakarta.servlet.ServletException;
- * import jakarta.servlet.http.HttpServletRequest; import
- * jakarta.servlet.http.HttpServletResponse;
- * 
- * @Configuration public class AuthenticationFilter extends OncePerRequestFilter
- * {
- * 
- * @Override protected void doFilterInternal(HttpServletRequest request,
- * HttpServletResponse response, FilterChain filterChain) throws
- * ServletException, IOException { if (request.getRequestURI().contains("login")
- * || request.getRequestURI().contains("logout") ||
- * request.getRequestURI().contains("reset")) { filterChain.doFilter(request,
- * response); return; }
- * 
- * if (request.getSession().getAttribute("username") == null) {
- * response.sendRedirect(request.getContextPath() + "/login"); return; }
- * filterChain.doFilter(request, response); }
- * 
- * }
- */
+
+package com.config;
+
+import java.io.IOException;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Configuration
+public class AuthenticationFilter extends OncePerRequestFilter {
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		String origin = request.getHeader("Origin");
+		if (origin != null && origin.equals("http://localhost:4200")) {
+			filterChain.doFilter(request, response);
+			return;
+		}
+
+		if (request.getRequestURI().contains("login") || request.getRequestURI().contains("logout")
+				|| request.getRequestURI().contains("reset")) {
+			filterChain.doFilter(request, response);
+			return;
+		}
+
+		if (request.getSession().getAttribute("username") == null) {
+			response.sendRedirect(request.getContextPath() + "/login");
+			return;
+		}
+		filterChain.doFilter(request, response);
+	}
+
+}
